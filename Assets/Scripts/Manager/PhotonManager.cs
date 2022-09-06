99c12d9cb9986b446231811f5837f40b5912ee3a
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-09-06 22:26:28 +0900

    [update]更新

--- Assets/Scripts/Manager/PhotonManager.cs
+++ Assets/Scripts/Manager/PhotonManager.cs
@@ -8,15 +8,52 @@ using System.Linq;
 
 public class PhotonManager : MonoBehaviour
 {
+    /// <summary>
+    /// ステータス
+    /// </summary>
+    public enum PhotonState
+    {
+        INIT,
+        CONNECTED,
+        IN_LOBBY,
+        READY,
+        IN_GAME,
+        DISCONNECTED,
+        WAITING,
+    }
+
+    #region Singleton
+    static PhotonManager _instance = null;
+
+    public static bool IsEmpty
+    {
+        get { return _instance == null; }
+    }
+
+    public static PhotonManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                System.Type type = typeof(PhotonManager);
+                _instance = GameObject.FindObjectOfType(type) as PhotonManager;
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
