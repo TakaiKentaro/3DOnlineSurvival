52be1c112261cc373d58bc1aceba8486b930a98a
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-09-10 17:06:28 +0900

    [add]UIRoomInfo・UIRoomList追加

---
+++ Assets/Scripts/Manager/Debug/UIRoomInfo.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+public class UIRoomInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
