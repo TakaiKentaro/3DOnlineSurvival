4cf3549fc0d8fef24eb2a5b509aba9d9fbb838ee
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-09-23 01:17:50 +0900

    [update]更新

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,30 @@ using Photon.Pun;
 using Photon.Realtime;
 public class PlayerController : MonoBehaviour
 {
-    PlayerStatus _status = new PlayerStatus();
+    #region Singleton
+    static PlayerController _instance = null;
+
+    public static bool IsEmpty
+    {
+        get { return _instance == null; }
+    }
+
+    public static PlayerController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                System.Type type = typeof(PhotonManager);
+                _instance = GameObject.FindObjectOfType(type) as PlayerController;
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
+    [SerializeField, Tooltip("Playerのステータス")] PlayerStatus _status = new PlayerStatus();
 
     
 }
