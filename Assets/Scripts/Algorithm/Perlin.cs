925d3c794a6a281932a1aae037e27568af7b1727
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-17 14:28:30 +0900

    [update]Perlin途中経過

--- Assets/Scripts/Algorithm/Perlin.cs
+++ Assets/Scripts/Algorithm/Perlin.cs
@@ -1,6 +1,7 @@
+using Unity.VisualScripting;
 using UnityEditor;
 
-public class Perlin
+class Perlin
 {
     /// <summary>
     /// x、y、z座標、オクターブ数、および進行度を取得し、それらを使用してPerlinノイズを生成
@@ -11,13 +12,13 @@ public class Perlin
     /// <param name="octaves"></param>
     /// <param name="persistence"></param>
     /// <returns></returns>
-    public static double OctavePerlin(double x, double y, double z, int octaves, double persistence)
+    public double OctavePerlin(double x, double y, double z, int octaves, double persistence)
     {
         double total = 0; // 合計値
         double frequency = 1; // 周波数
         double amplitude = 1; // 振れ幅
-         
-            // x、y、z座標に対してPerlinノイズを生成。その後、生成されたPerlinノイズの値がamplitudeによって乗算され、その結果がtotalに加算
+
+        // x、y、z座標に対してPerlinノイズを生成。その後、生成されたPerlinノイズの値がamplitudeによって乗算され、その結果がtotalに加算
         // 次に、amplitudeがpersistenceによって乗算され、frequencyは2倍になる。
         // よって、より高周波数のPerlinノイズが生成され、より小さなアンプリフィケーションが適用される。
         // オクターブのような概念を実現し、より詳細なPerlinノイズを生成する
@@ -56,6 +57,13 @@ public class Perlin
     /// </summary>
     private static readonly int[] p;
 
+    public int repeat;
+
+    public Perlin(int repeat = -1)
+    {
+        this.repeat = this.repeat;
+    }
+
     /// <summary>
     /// x、y、z座標を使用してPerlinノイズを生成
     /// </summary>
@@ -67,16 +75,104 @@ public class Perlin
             p[x] = permutation[x % 512];
         }
     }
-    
-    public static double perlin(double x, double y, double z)
+
+    /// <summary>
+    /// repeat "変数が0より大きいかどうかをチェックし、大きい場合はx、y、zの値を "repeat "で割った余りを計算し、その結果をそれぞれx、y、zに割り戻している
+    /// 座標が特定の範囲に収まるように包み込む
+    /// 効果的にノイズパターンを繰り返させることができる
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    /// <returns></returns>
+    public double perlin(double x, double y, double z)
     {
-        /*if (repeat > 0)
+        if (repeat > 0)
         {
             x = x * repeat;
             y = y * repeat;
             z = z * repeat;
-        }*/
+        }
 
+        // 点 (x, y, z) が位置する「単位立方体」を計算するために使用
+        // 点 (x, y, z) におけるパーリンノイズ値を生成するために使用される,並べ替え配列の値を検索するためのインデックスとして使用
+        // 並べ換え配列は256の要素（0-255）しかないため、ビット演算子&255を適用することで、座標の整数表現の最後の8ビットを効果的に取得することができる
+        // これでこれらの座標をインデックスとして使って並べ換え配列の中の値を調べることができる
+        int xi = (int)x & 255;
+        int yi = (int)y & 255;
+        int zi = (int)z & 255;
+
+        // 座標の整数部分を元の座標から引くことによって、座標の小数部分（x, y, z）を計算
+        // まず x,y,z を int にキャストし、それぞれ x,y,z から引き算する
+        // 変数 xf,yf,zfは座標(x, y, z)の小数部分を含み点(x,y,z)における最終的なパーリンノイズ値を生成するための補間処理に使用される
+        // 補間は、2つの既知の値間の値を推定するために使用される手法で、この場合は、点 (x, y, z) が位置する単位立方体の角におけるパーリンノイズ値である
+        // xf,yf,zfに格納された値は、点(x,y,z)の値と次の単位立方体に位置する点(x+1,y+1,z+1)の値の間の滑らかな遷移を計算するために使用される
+        double xf = x - (int)x;
+        double yf = y - (int)y;
+        double zf = z - (int)z;
+
+        // 単位立方体の異なる部分におけるノイズ値の遷移を滑らかにするために、座標の小数部分(xf,yf,zf)にfade()関数を適用する
+        // 単位立方体の角におけるノイズ値間の遷移を滑らかにすることで、補間処理を滑らかにするために使用される
+        // 座標の分数部分（xf, yf, zf）にそれぞれ適用され、変数 u, v, w となる
+        double u = fade(xf);
+        double v = fade(yf);
+        double w = fade(zf);
+
+        // 単位立方体内の点 (x, y, z) の並べ替えられたインデックスを計算、これらの並べ替えインデックスは、点 (x, y, z) におけるノイズ値を生成するために使用します
+        // xi、yi、zi座標（先に計算）をインデックスとして使用し、並べ替え配列の値を検索し、permutation[a]の値がziに追加され、インデックスaaが得られる
+        // この処理を繰り返し、aに1を加え、ziを加えてインデックスabを得る
+        // 次に、この処理を繰り返して、xiに1を加え、yiを加えて添字bを得て、この処理を再び繰り返し、permutation[b]とpermutation[b+1]にそれぞれziを加えてインデックスbaとbbを得る
+        // 並べ替えられたインデックスaa、ab、ba、bbは、点（x、y、z）におけるノイズ値を計算するために使用される並べ替え配列の値を調べるために使用する
+        int a = p[xi] + yi;
+        int aa = p[a] + zi;
+        int ab = p[a + 1] + zi;
+        int b = p[xi + 1] + yi;
+        int ba = p[b] + zi;
+        int bb = p[b + 1] + zi;
+
+        double x1, x2, y1, y2;
+        x1 = lerp(grad(p[aa], xf, yf, zf),
+            grad(p[ba], xf - 1, yf, zf), u);
+        x2 = lerp(grad(p[ab], xf, yf - 1, zf),
+            grad(p[bb], xf - 1, yf - 1, zf),
+            u);
+        y1 = lerp(x1, x2, v);
+
+        x1 = lerp(grad(p[aa + 1], xf, yf, zf - 1),
+            grad(p[ba + 1], xf - 1, yf, zf - 1), u);
+        x2 = lerp(grad(p[ab + 1], xf, yf - 1, zf - 1),
+            grad(p[bb + 1], xf - 1, yf - 1, zf - 1), u);
+        y2 = lerp(x1, x2, v);
+
+        return (lerp(y1, y2, w) + 1) / 2;
+    }
+
+    public static double grad(int hash, double x, double y, double z)
+    {
         return 0;
+    }
+
+    /// <summary>
+    /// 単位立方体の角のノイズ値の間の遷移を滑らかにすることで、補間処理を滑らかにするために使用される
+    /// 入力値(t)を滑らかなエルミート曲線に写像し、単位立方体の異なる部分におけるノイズ値の間の遷移を滑らかにするもの
+    /// 6t^5 - 15t^4 + 10t^3 の多項式で定義され、補間処理で重み付け係数として使用される0から1への滑らかな遷移を作成する
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static double fade(double t)
+    {
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+
+    /// <summary>
+    /// 与えられた分数 "x" で線形補間するlerpメソッド
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static double lerp(double a, double b, double x)
+    {
+        return a + x * (b - a);
     }
 }
