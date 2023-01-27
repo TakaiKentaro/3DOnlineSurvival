f39c0b5996b038544a1d770d1413496d49220b05
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-27 17:23:16 +0900

    [update]UI_Item.cs完成(仮)

--- Assets/Scripts/CraftingSystem/UI_Item.cs
+++ Assets/Scripts/CraftingSystem/UI_Item.cs
@@ -28,6 +28,75 @@ public class UI_Item : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IE
     {
         _canvasGroup.alpha = 0.5f;
         _canvasGroup.blocksRaycasts = false;
-        UI_Ite
+        //UI_ItemDrag.Instance.Show(_item);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //_rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.alpha = 1f;
+        _canvasGroup.blocksRaycasts = true;
+        //UI_ItemDrag.Instance.Hide();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            if (_item != null)
+            {
+                /*if (_item.IsStackable())
+                {
+                    if (_item.amount > 1)
+                    {
+                        if (_item.GetItemHolder().CanAddItem())
+                        {
+                            int splitAmount = Mathf.FloorToInt(_item.amount / 2f);
+                            _item.amount -= splitAmount;
+                            Item duplicateItem = new Item { itemType = _item.itemType, amount = splitAmount };
+                            _item.GetItemHolder().AddItem(duplicateItem);
+                        }
+                    }
+                }*/
+            }
+        }
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        _image.sprite = sprite;
+    }
+
+    public void SetAmountText(int amount)
+    {
+        if (amount <= 1)
+        {
+            _amountText.text = "";
+        }
+        else
+        {
+            _amountText.text = amount.ToString();
+        }
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ShowItem(Item item)
+    {
+        this._item = item;
+        //SetSprite(Item.GetSprite(item.itemType));
+        //SetAmountText(item.amount);
     }
 }
