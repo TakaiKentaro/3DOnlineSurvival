2264c55a10ea0c0bd0a1c1b1e189d768fb4a6a7a
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-27 17:06:58 +0900

    [add]UI_ItemDrag.cs作成

--- Assets/Scripts/CraftingSystem/UI_Item.cs
+++ Assets/Scripts/CraftingSystem/UI_Item.cs
@@ -1,8 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class UI_Item : MonoBehaviour
+public class UI_Item : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    
+    private Canvas _canvas;
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    private Image _image;
+    private Item _item;
+    private TextMeshProUGUI _amountText;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvas = GetComponent<Canvas>();
+        _image = transform.Find("Image").GetComponent<Image>();
+        _amountText = transform.Find("AmountText").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.alpha = 0.5f;
+        _canvasGroup.blocksRaycasts = false;
+        UI_Ite
+    }
 }
