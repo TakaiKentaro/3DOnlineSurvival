21447d467ab926943638ef37d3977845891cbca2
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-30 20:28:25 +0900

    [update]CraftingSystem.cs途中経過

--- Assets/Scripts/CraftingSystem/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem/CraftingSystem.cs
@@ -1,18 +1,94 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon.StructWrapping;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CraftingSystem : MonoBehaviour
+public class CraftingSystem : IItemHolder
 {
-    // Start is called before the first frame update
-    void Start()
+    public const int GRID_SIZE = 5;
+
+    public event EventHandler OnGridChange;
+
+    private Dictionary<Item.ItemType, Item.ItemType[,]> _recipeDictionary;
+
+    private Item[,] _itemArray;
+    private Item _outputItem;
+
+
+    public bool IsEmpty(int x,int y)
     {
-        
+        return _itemArray[x, y] == null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Item GetItem(int x, int y)
     {
-        
+        return _itemArray[x, y];
     }
+
+    public void SetItem(Item item, int x, int y)
+    {
+        if (!object.ReferenceEquals(item, null))
+        {
+            item.RemoveFromItemHolder();
+            item.SetItemHolder(this);
+        }
+        _itemArray[x, y] = item;
+        //CreateOutput();
+        OnGridChange?.Invoke(this,EventArgs.Empty);
+    }
+
+    public void IncreaseItemAmount(int x, int y)
+    {
+        GetItem(x, y).amount++;
+        OnGridChange?.Invoke(this,EventArgs.Empty);
+    }
+
+    public void DecreaseItemAmount(int x, int y)
+    {
+        if (!object.ReferenceEquals(GetItem(x, y), null))
+        {
+            GetItem(x, y).amount--;
+            if (object.ReferenceEquals(GetItem(x, y), 0))
+            {
+                RemoveItem(x, y);
+            }
+            OnGridChange?.Invoke(this,EventArgs.Empty);
+        }
+    }
+
+    public void RemoveItem(int x, int y)
+    {
+        SetItem(null,x,y);
+    }
+
+    public bool TryAddItem(Item item, int x, int y)
+    {
+        if (IsEmpty(x, y))
+        {
+            SetItem(item,x,y);
+            return true;
+        }
+        else
+        {
+            if (object.ReferenceEquals(item.itemType, GetItem(x, y).itemType))
+            {
+                IncreaseItemAmount(x,y);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+    
+
+
+
+
+
+
 }
