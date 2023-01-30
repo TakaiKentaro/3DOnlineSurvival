64ae1e779d1b1a505fb6ed3b330f3b2dc5a5b7e2
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-30 19:41:15 +0900

    [update]UI_ItemDrag.cs完成(仮)

--- Assets/Scripts/CraftingSystem/UI_ItemDrag.cs
+++ Assets/Scripts/CraftingSystem/UI_ItemDrag.cs
@@ -1,18 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_ItemDrag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UI_ItemDrag Instance { get; private set; }
+
+    private Canvas _canvas;
+    private RectTransform _rectTransform;
+    private RectTransform _parentRectTransform;
+    private CanvasGroup _canvasGroup;
+    private Image _image;
+    private Item _item;
+    private TextMeshProUGUI _amountText;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvas = GetComponentInParent<Canvas>();
+        _image = transform.Find("Image").GetComponent<Image>();
+        _amountText = transform.Find("AmountText").GetComponent<TextMeshProUGUI>();
+        _parentRectTransform = transform.parent.GetComponent<RectTransform>();
+
+        //Hide();
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public Item GetItem()
     {
+        return _item;
+    }
+
+    public void SetItem(Item item)
+    {
+        this._item = item;
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        _image.sprite = sprite;
+    }
+
+    public void SetAmountText(int amount)
+    {
+        if (amount <= 1)
+        {
+            _amountText.text = "";
+        }
+        else
+        {
+            _amountText.text = amount.ToString();
+        }
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show(Item item)
+    {
+        gameObject.SetActive(true);
         
+        SetItem(item);
+        SetSprite(item.GetSprite());
+        SetAmountText(item.amount);
+        UpdatePosition();
     }
 }
