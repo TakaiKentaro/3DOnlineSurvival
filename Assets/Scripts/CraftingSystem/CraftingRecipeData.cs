895acccf8031cc5cd1426409e7985ccc8a83073b
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-01-31 16:57:02 +0900

    [add]CraftingRecipe.cs作成

---
+++ Assets/Scripts/CraftingSystem/CraftingRecipeData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "ScriptableObject", fileName = "CraftRecipeData" )]
+public class CraftingRecipeData : ScriptableObject
+{
+    private CraftingSystem _craftingSystem;
+    
+    
+}
