636fa14454bda991192bc4a8117522f22a5a82ae
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-02-24 16:40:55 +0900

    [update]カメラを追従するUI完成

--- Assets/Scripts/InGame/Object/Object_UI.cs
+++ Assets/Scripts/InGame/Object/Object_UI.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Object_UI : MonoBehaviour
 {
     [SerializeField] float _hideDistance = 0;
+    [SerializeField] Camera _camera;
 
     private void Update()
     {
-        var camera = Vector3.Distance(Camera.main.transform.position, transform.position);
+        var camera = Vector3.Distance(_camera.transform.position, transform.position);
+
+        if(camera <= _hideDistance)
+        {
+            Vector3 vector3 = _camera.transform.position - this.transform.position;
+            Quaternion quaternion = Quaternion.LookRotation(vector3);
+            this.transform.rotation = quaternion;
+        }
     }
 }
