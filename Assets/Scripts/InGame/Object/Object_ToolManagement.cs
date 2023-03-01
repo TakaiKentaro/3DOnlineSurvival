df33a9765b818a4a6a15882a86a6588c01df20a6
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-03-01 23:49:54 +0900

    [add]Object_ToolManagement.cs作成

---
+++ Assets/Scripts/InGame/Object/Object_ToolManagement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_ToolManagement : MonoBehaviour
+{
+    [SerializeField] private Object_Tool _sword;
+    [SerializeField] private Object_Tool _axe;
+    [SerializeField] private Object_Tool _pickaxe;
+
+
+}
